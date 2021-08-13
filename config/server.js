5023bbc98f09779d14bd472a6dbf33cfe8bf780b
David Diaz <david@d2weblogic.com>
2021-08-12 19:19:55 -0600

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '09cf42798cefd0e4ee5fd55ebd385529'),
+    },
+  },
+});
